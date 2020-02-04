d0c34d80055ad50fec4369e7e5dfca0906496e30
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-02-04 10:18:53 +0000

    [Update]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,17 +1,39 @@
 class HomesController < ApplicationController
 	def index
-	end
-
-	def show
+		@books = Book.all
 	end
 
 	def new
 		@book = Book.new
 	end
 
+	def show
+		@book = Book.find(params[:id])
+	end
+
 	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to home_path(book.id)
 	end
 
 	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
+		redirect_to home_path(book.id)
+	end
+
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to homes_path
+	end
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-	resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'homes#top'
+  get 'homes/new' => 'homes#new'
+  get 'homes' => 'homes#index', as: 'homes'
+  post 'homes' => 'homes#create'
+  get 'homes/:id' => 'homes#show', as: 'home'
+  get 'homes/:id/edit' => 'homes#edit', as: 'edit_home'
+  patch 'homes/:id' => 'homes#update', as: 'update_home'
+  delete 'homes/:id' => 'homes#destroy', as: 'destroy_home'
 end
