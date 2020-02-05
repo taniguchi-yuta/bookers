170223a444ece5744e69b294eed8c614ddfc4a37
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-02-05 10:24:57 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html.
   get '/' => 'homes#top'
   get 'homes/new' => 'homes#new'
   get 'homes' => 'homes#index', as: 'homes'
